5bb1de775468d9426ef1a54d221f6b8d2890b731
masa-ymd <masa.ymd.0703@gmail.com>
2022-07-22 18:57:09 +0900

    add meanpooling

--- train.py
+++ train.py
@@ -117,6 +117,18 @@ df['discourse_effectiveness'] = encoder.fit_transform(df['discourse_effectivenes
 with open(f"{MODEL_PATH}/le.pkl", "wb") as fp:
     joblib.dump(encoder, fp)
 
+class MeanPooling(nn.Module):
+    def __init__(self):
+        super(MeanPooling, self).__init__()
+        
+    def forward(self, last_hidden_state, attention_mask):
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
+        sum_embeddings = torch.sum(last_hidden_state * input_mask_expanded, 1)
+        sum_mask = input_mask_expanded.sum(1)
+        sum_mask = torch.clamp(sum_mask, min=1e-9)
+        mean_embeddings = sum_embeddings / sum_mask
+        return mean_embeddings
+
 class FeedBackDataset(Dataset):
     def __init__(self, df, tokenizer, max_length):
         self.df = df
