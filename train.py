9a7c19b40ae6744b2ce109066d55a5aa47637f64
masa-ymd <masa.ymd.0703@gmail.com>
2022-07-27 03:03:10 +0900

    increase batch size

--- train.py
+++ train.py
@@ -79,10 +79,10 @@ if not os.path.exists(MODEL_PATH):
     os.makedirs(f"{MODEL_PATH}/tokenizer")
 
 CONFIG = {"seed": 2022,
-          "epochs": 1,
+          "epochs": 3,
           #"model_name": "microsoft/deberta-v3-base",
           "model_name": "microsoft/deberta-v3-large",
-          "train_batch_size": 16,
+          "train_batch_size": 32,
           "valid_batch_size": 16,
           "max_length": 512,
           #"learning_rate": 1e-5, #3e-5, 2e-5
