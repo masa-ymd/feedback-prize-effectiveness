5e608fd45f67666113dd0a31198f14d105d64f34
masa-ymd <masa.ymd.0703@gmail.com>
2022-07-30 04:27:15 +0900

    dynamic padding

--- train.py
+++ train.py
@@ -287,7 +287,41 @@ class FeedBackDataset(Dataset):
             'target': self.targets[index]
         }
 
-collate_fn = DataCollatorWithPadding(tokenizer=CONFIG['tokenizer'])
+# Dynamic Padding (Collate)
+class Collate:
+    def __init__(self, tokenizer, isTrain=True):
+        self.tokenizer = tokenizer
+        self.isTrain = isTrain
+        # self.args = args
+
+    def __call__(self, batch):
+        output = dict()
+        output["input_ids"] = [sample["input_ids"] for sample in batch]
+        output["attention_mask"] = [sample["attention_mask"] for sample in batch]
+        if self.isTrain:
+            output["target"] = [sample["target"] for sample in batch]
+
+        # calculate max token length of this batch
+        batch_max = max([len(ids) for ids in output["input_ids"]])
+
+        # add padding
+        if self.tokenizer.padding_side == "right":
+            output["input_ids"] = [s + (batch_max - len(s)) * [self.tokenizer.pad_token_id] for s in output["input_ids"]]
+            output["attention_mask"] = [s + (batch_max - len(s)) * [0] for s in output["attention_mask"]]
+        else:
+            output["input_ids"] = [(batch_max - len(s)) * [self.tokenizer.pad_token_id] + s for s in output["input_ids"]]
+            output["attention_mask"] = [(batch_max - len(s)) * [0] + s for s in output["attention_mask"]]
+
+        # convert to tensors
+        output["input_ids"] = torch.tensor(output["input_ids"], dtype=torch.long)
+        output["attention_mask"] = torch.tensor(output["attention_mask"], dtype=torch.long)
+        if self.isTrain:
+            output["target"] = torch.tensor(output["target"], dtype=torch.long)
+
+        return output
+
+#collate_fn = DataCollatorWithPadding(tokenizer=CONFIG['tokenizer'])
+collate_fn = Collate(CFG.tokenizer)
 
 class FeedBackModel(nn.Module):
     def __init__(self, model_name, tokenizer):
