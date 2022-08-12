29cb407414d85d0664619468e247a1f7b7345872
masa-ymd <masa.ymd.0703@gmail.com>
2022-08-12 12:16:39 +0900

    fix bug

--- train2.py
+++ train2.py
@@ -374,12 +374,13 @@ class FeedBackModel(nn.Module):
     ):        
         out = self.model(input_ids=input_ids,attention_mask=attention_mask,
                          output_hidden_states=output_hidden_states)
-        #cnn_out = F.relu(self.cnn1(out.last_hidden_state.permute(0, 2, 1)))
-        #cnn_out = self.cnn2(cnn_out)
+        cnn_out = F.relu(self.cnn1(out.last_hidden_state.permute(0, 2, 1)))
+        cnn_out = self.cnn2(cnn_out)
         lstm_out, _ = self.lstm(out.last_hidden_state, None)
+        sequence_out = lstm_out[:, -1, :]
         #cat_out = torch.cat([out["hidden_states"][-1*i][:,0] for i in range(1, 4+1)], dim=1)  # concatenate
         #pool_out = self.pooler(out.last_hidden_state, attention_mask)
-        logits = sum([self.fc(dropout(lstm_out)) for dropout in self.dropouts]) / config.num_msd
+        logits = sum([self.fc(dropout(sequence_out)) for dropout in self.dropouts]) / config.num_msd
         
         loss = None
         if labels is not None:
