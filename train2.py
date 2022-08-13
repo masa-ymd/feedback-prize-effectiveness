9544fa156bc941f2331ba2a3d58583dd49318718
masa-ymd <masa.ymd.0703@gmail.com>
2022-08-13 15:24:19 +0900

    debug

--- train2.py
+++ train2.py
@@ -385,6 +385,7 @@ class FeedBackModel(nn.Module):
         #pool_out = self.pooler(out.last_hidden_state, attention_mask)
         #logits = sum([self.fc(dropout(sequence_out)) for dropout in self.dropouts]) / config.num_msd
         print(f"cnn_out2: {self.cnn2(cnn_out).size()}")
+        print(f"{[self.cnn2(dropout(cnn_out)).size() for dropout in self.dropouts]}")
         logits = sum([self.cnn2(dropout(cnn_out)) for dropout in self.dropouts]) / config.num_msd
         
         loss = None
