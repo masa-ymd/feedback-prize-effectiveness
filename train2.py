41dfd20ffe74f40fa42fb533df6751b2c3ba0ac5
masa-ymd <masa.ymd.0703@gmail.com>
2022-08-13 19:11:14 +0900

    debug

--- train2.py
+++ train2.py
@@ -397,9 +397,9 @@ class FeedBackModel(nn.Module):
         return TokenClassifierOutput(
             logits=logits,
             loss=loss,
-            last_hidden_state=out.last_hidden_state,
-            attentions=out.attentions,
-            hidden_states=out.hidden_states
+            last_hidden_state=out['last_hidden_state'],
+            attentions=out['attentions'],
+            hidden_states=out.['hidden_states']
         )
 
 def criterion(res):
