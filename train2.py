b74f6303da3ee12528683103fc9e93d86f1a9786
masa-ymd <masa.ymd.0703@gmail.com>
2022-08-14 03:16:42 +0900

    fix bug

--- train2.py
+++ train2.py
@@ -389,7 +389,7 @@ class FeedBackModel(nn.Module):
         #logits = sum([self.fc(dropout(sequence_out)) for dropout in self.dropouts]) / config.num_msd
         #print(f"cnn_out2: {self.cnn2(cnn_out).size()}")
         #print(f"{[self.cnn2(dropout(cnn_out)).size() for dropout in self.dropouts]}")
-        logits = sum([self.fc(dropout(pool_out)) for dropout in self.dropouts]) / config.num_msd
+        #logits = sum([self.fc(dropout(pool_out)) for dropout in self.dropouts]) / config.num_msd
         logits = sum([dropout(pool_out) for dropout in self.dropouts]) / config.num_msd
         #print(out)
         
