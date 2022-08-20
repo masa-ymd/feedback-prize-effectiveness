9186d13131b85a951d91d2b6a01047aaef4eae9a
masa-ymd <masa.ymd.0703@gmail.com>
2022-08-20 12:02:03 +0900

    debug

--- train3.py
+++ train3.py
@@ -574,6 +574,7 @@ class FeedBackModel(nn.Module):
         #self.cnn2 = nn.Conv1d(256, 3, kernel_size=2, padding=1)
         #self.lstm = nn.LSTM(self.config.hidden_size, self.config.hidden_size, batch_first=True, bidirectional=True)
         self.fc = nn.Linear(self.config.hidden_size, 3)
+        self.fc2 = nn.Linear([25, self.config.hidden_size], 3)
         #self.fc = nn.Linear(514, 3)
         self.dropouts = nn.ModuleList([nn.Dropout(0.2) for _ in range(config.num_msd)])
         
@@ -611,7 +612,7 @@ class FeedBackModel(nn.Module):
         #print(f"cnn_out2: {self.cnn2(cnn_out).size()}")
         #print(f"{[self.cnn2(dropout(cnn_out)).size() for dropout in self.dropouts]}")
         # MIXOUT!
-        logits = sum([self.fc(dropout(pool_out)) for dropout in self.dropouts]) / config.num_msd
+        logits = sum([self.fc2(dropout(pool_out)) for dropout in self.dropouts]) / config.num_msd
         logits2 = sum([self.fc(dropout(pool_out2)) for dropout in self.dropouts]) / config.num_msd
         print(f"label {labels.size()}")
         print(f"logits {logits.size()}")
